b7486efb17ae8b51ba2afbc8589b48774ff65eb5
aabhishek454 <164495596+aabhishek454@users.noreply.github.com>
2026-08-22 02:48:32 +0530

    v1.9.3: service worker cache bump v193

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='abhi-music-v192';
-const SHELL=['/','/abhi-music-v17.css','/abhi-music-v17.js','/abhi-music-v19-fix.js?v=192','/favicon.svg','/manifest.webmanifest'];
+const CACHE='abhi-music-v193';
+const SHELL=['/','/abhi-music-v17.css','/abhi-music-v17.js','/abhi-music-v19-fix.js?v=193','/favicon.svg','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(event.request.method!=='GET'||url.pathname.startsWith('/api/'))return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))) });
